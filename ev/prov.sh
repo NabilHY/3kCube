b3b95288d3c4f98762f52ff792a89ff1ba5b67ca
NabilHY <nabil.hayun@gmail.com>
2026-08-23 23:35:33 +0100

    ev vm

---
+++ ev/prov.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -eou pipefail
+
+#sudo dnf update -y
+
+echo "install git"
+dnf install -y git libevent-devel bison flex make cmake gettext/g
+
+echo "installing neovim"
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+rm -rf /opt/nvim-linux-x86_64
+tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+echo "setup lazyvim env ..."
+git clone https://github.com/NabilHY/tmux-nvim-config.git
+
+echo "installing tmux ..."
+git clone https://github.com/tmux/tmux.git
+cd tmux
+sh autogen.sh
+./configure
+make && sudo make install
+
+cd "$HOME"
+
+echo "setup tmux config"
+ln -sf "$HOME/tmux-nvim-config/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "/opt/nvim-linux-x86_64/bin/nvim" "/usr/local/bin/nvim"
+
+echo "setup nvim config"
+mkdir -p "$HOME/.config/nvim"
+cp -rf "$HOME/tmux-nvim-config/nvim/" "$HOME/.config/"
+
+echo "VM Provisioned ..."
